b09a63d8db5df4c1b7b34dac002e5439987ba346
zobair.hu <zobair.hu@gmail.com>
2022-03-18 03:34:12 +0600

    Add project files.

---
+++ TestNinja.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("684cdf8f-6f83-4d6e-8179-0cbab484cf32")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestNinja.UnitTest/ReservationTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelldBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+        [Test]
+        public void CanBeCancelldBy_SameUserCancellingReservation_ReturnsTrue()
+        {
+            //Arrange
+            User user = new User();
+            var reservation = new Reservation{ MadeBy = user };
+
+            //Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+        [Test]
+        public void CanBeCancelldBy_AnotherUserCancellingReservation_ReturnsFalse()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            //Assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
